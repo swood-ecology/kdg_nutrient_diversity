135c87ebfe6a961b52a27c86090f5affc56ba891
Stephen Wood <stephen.wood@CT201-SWOOD.wireless.yale.internal>
2017-07-25 14:14:18 -0400

    Minor formatting changes

--- hh_nutrient_production_diversity.R
+++ hh_nutrient_production_diversity.R
@@ -1,7 +1,8 @@
-library(haven)    # For working with Stata data
-library(RStata)   # For working with Stata data
-library(FD)       # For calculating FD metrics
-source("~/Box Sync/Work/Statistics/Code/setRows.R")  # for setting rows
+library(haven)                                        # For working with Stata data
+library(RStata)                                       # For working with Stata data
+library(FD)                                           # For calculating FD metrics
+library(stargazer)                                    # To export html code for regression tables
+source("~/Box Sync/Work/Statistics/Code/setRows.R")   # for setting rows
 
 # Production data
 RICARDIANDATA <- read_dta("~/Box Sync/Work/Writing/Manuscripts/Submitted/Fonio nutritional diversity/RICARDIANDATA.dta")
@@ -42,13 +43,54 @@ hh <- hh[,order(colnames(hh))]
 diversity_scores <- dbFD(nutr,hh,calc.CWM=F,calc.FGR=T) # 4 groups
 diversity_data_frame <- data.frame(as.factor(diversity_scores$nbsp),diversity_scores$FDis,diversity_scores$FDiv,diversity_scores$FEve,diversity_scores$FRic)
 names(diversity_data_frame) <- c("SpeciesNumber","FDis","FDiv","FEve","FRic")
-pairs(diversity_data_frame[,-1], upper.panel=NULL)
 
-par(mfrow=c(2,2))
+# Compare metrics to PNA
+pna <- read.csv("~/Box Sync/Work/Writing/Manuscripts/Submitted/Fonio nutritional diversity/J Appl Ecol Submission/PNA Analyses/PNA.csv")
+diversity_data_frame <- cbind(diversity_data_frame,pna)
+
+# Statistical tests based on ANOVA
+# Create regression table
+fdis.model <- lm(FDis~SpeciesNumber,data=diversity_data_frame)
+fdiv.model <- lm(FDiv~SpeciesNumber,data=diversity_data_frame)
+feve.model <- lm(FEve~SpeciesNumber,data=diversity_data_frame)
+fric.model <- lm(FRic~SpeciesNumber,data=diversity_data_frame)
+pna.model <- lm(PNA~SpeciesNumber,data=diversity_data_frame)
+
+stargazer(fdis.model,fdiv.model,feve.model,fric.model,pna.model,type="html",
+          style="aer",omit.stat="f",
+          dep.var.labels = c("Functional Dispersion","Functional Divergence",
+                             "Functional Evenness","Functional Richness",
+                             "Potential Nutrient Adequacy"),
+          covariate.labels = c("Two crops","Three crops","Four crops"),
+          out="~/Box Sync/Work/Writing/Manuscripts/Submitted/Fonio nutritional diversity/J Appl Ecol Submission/RegTable.html")
+
+# Make plots
+# Create function to determine correlations to plot on upper panels
+panel.cor <- function(x, y, digits = 2, cex.cor, ...)
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  r <- abs(cor(x, y,use = "complete.obs"))
+  txt <- format(c(r, 0.123456789), digits = digits)[1]
+  if(missing(cex.cor)) cex.cor <- 1/(strwidth(txt))
+  cex.final = cex.cor * r
+  if(cex.final < .5) cex.final <- .6
+  text(0.5, 0.5, txt, cex = cex.final)
+}
+
+# Plot diversity against species number
+par(mfrow=c(3,2))
 boxplot(FDis~SpeciesNumber,data=diversity_data_frame,ylab="Functional dispersion (FDis)")
 boxplot(FDiv~SpeciesNumber,data=diversity_data_frame,ylab="Functional divergence (FDiv)")
 boxplot(FEve~SpeciesNumber,data=diversity_data_frame,ylab="Functional evenness (FEve)")
 boxplot(FRic~SpeciesNumber,data=diversity_data_frame,ylab="Functional richness (FRic)")
+boxplot(PNA~SpeciesNumber,data=diversity_data_frame,ylab="Potential Nutrient Adequacy (PNA)")
+par(mfrow=c(1,1))
+
+
+# Plot correlations
+pairs(diversity_data_frame[,-c(1,6,8:12)], upper.panel = panel.cor)
+
 
 # Simulations
 diversity_simulations <- simul.dbFD(s=c(3, 5, 10, 15),t=20, r=200, p=15, 
